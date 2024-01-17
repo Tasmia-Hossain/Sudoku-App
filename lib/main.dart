65ed8df9bec5dd72fe24c13b02bc27aba72a12bb
Tasmia Hossain <tasmiahossainkashfia@gmail.com>
2024-01-17 21:21:01 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:sudoku/game_mode.dart';
 import 'package:sudoku/home_page.dart';
 import 'package:sudoku/loading_page.dart';
 
+import 'game_screen.dart';
+
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -10,7 +12,8 @@ void main() {
     routes: {
       'loading_page':(context)=>Loading(),
       'home_page':(context)=>Homepage(),
-      'game_mode':(context)=>Mode(gameMode: '')
+      'game_mode':(context)=>Mode(gameMode: ''),
+      'game_screen':(context)=>GameScreen()
     },
   ));
 }

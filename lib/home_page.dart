ba3c446a4492f00d7da04bc23455db1792d218e8
Tasmia Hossain <tasmiahossainkashfia@gmail.com>
2024-03-29 23:56:29 +0600

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,70 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:sudoku/game_mode.dart';
-
+import 'package:flutter/services.dart';
+import 'package:init_project/settings_screen.dart';
+import 'DatabaseClass.dart';
+import 'SelectDifficulty.dart';
 import 'game_screen.dart';
+import 'instruction.dart';
 
-class Homepage extends StatefulWidget {
-  const Homepage({Key? key}) : super(key: key);
-
-  @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
+class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(''),
+        automaticallyImplyLeading: false,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'SUDOKU',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 15),
             Image.network(
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM8Hew8cRa8Gk5FOw5GMbbf1Numi4bWXGx-A&usqp=CAU',
+              'https://raw.githubusercontent.com/abu-dojana/SudoSolver-Bliss/master/sudokubliss.png',
               height: 150,
             ),
             SizedBox(height: 30),
             ElevatedButton(
               onPressed: () {
-                // Navigate to the game screen
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => GameScreen()),
+                  MaterialPageRoute(builder: (context) => DatabaseClass()),
                 );
               },
-              child: Text('Continue Game'),
+              child: Text('Load Game'),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 30),
             ElevatedButton(
               onPressed: () {
-                // Show the game mode selection bottom sheet
-                _showGameModeBottomSheet(context);
+                Times t = new Times();
+                t.setTime(0);
+                t.setBoard("[]");
+                t.setId(0);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Menu()),
+                );
               },
               child: Text('New Game'),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Navigate to instructions screen
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => InstructionsScreen()),
-                );
-              },
-              child: Text('How to play'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to settings screen
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => SettingsScreen()),
@@ -72,93 +56,28 @@ class _HomepageState extends State<Homepage> {
               },
               child: Text('Settings'),
             ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => InstructionsScreen()),
+                );
+              },
+              child: Text('Instructions'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Exit the app
+                SystemNavigator.pop();
+              },
+              child: Text('Exit'),
+            ),
           ],
         ),
       ),
     );
   }
-
-  void _showGameModeBottomSheet(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return Container(
-          padding: EdgeInsets.all(16),
-          width: double.infinity, // Set width to match screen width
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  // Start a new game with easy mode
-                  _navigateToMode(context, 'Easy');
-                },
-                child: Text('Easy'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Start a new game with medium mode
-                  _navigateToMode(context, 'Medium');
-                },
-                child: Text('Medium'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Start a new game with hard mode
-                  _navigateToMode(context, 'Hard');
-                },
-                child: Text('Hard'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Start a new game with expert mode
-                  _navigateToMode(context, 'Expert');
-                },
-                child: Text('Expert'),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  void _navigateToMode(BuildContext context, String gameMode) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => Mode(gameMode: gameMode)),
-    );
-  }
 }
 
-class SettingsScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sudoku Game - Settings'),
-      ),
-      body: Center(
-        child: Text('Settings Screen'),
-      ),
-    );
-  }
-}
-
-class InstructionsScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sudoku Game - Instructions'),
-      ),
-      body: Center(
-        child: Text('Instructions Screen'),
-      ),
-    );
-  }
-}

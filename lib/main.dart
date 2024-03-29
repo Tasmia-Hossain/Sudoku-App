2409e289e8b748ccd24493bc09092e6991529ee6
Tasmia Hossain <tasmiahossainkashfia@gmail.com>
2024-03-29 23:54:29 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:sudoku/game_mode.dart';
-import 'package:sudoku/home_page.dart';
-import 'package:sudoku/loading_page.dart';
-
-import 'game_screen.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:init_project/DatabaseClass.dart';
+import 'package:init_project/game_screen.dart';
+import 'package:init_project/instruction.dart';
+import 'package:init_project/settings_screen.dart';
+import 'package:init_project/SelectDifficulty.dart';
+import 'home_page.dart';
+import 'loading_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: 'loading_page',
-    routes: {
-      'loading_page':(context)=>Loading(),
-      'home_page':(context)=>Homepage(),
-      'game_mode':(context)=>Mode(gameMode: ''),
-      'game_screen':(context)=>GameScreen()
-    },
-  ));
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveTheme(
+      light: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      dark: ThemeData.dark(),
+      initial: AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        debugShowCheckedModeBanner: false, // Set this to false
+        initialRoute: 'loading_page',
+        routes: {
+          'loading_page': (context) => Loading(),
+          'home_page': (context) => Homepage(),
+          'game_mode': (context) => Menu(),
+          'settings_screen': (context) => SettingsScreen(),
+          'game_screen': (context) => GameScreen(),
+          'DatabaseClass': (context) => DatabaseClass(),
+          'instruction': (context) => InstructionsScreen(),
+        },
+        theme: theme,
+        darkTheme: darkTheme,
+      ),
+    );
+  }
+}
+
